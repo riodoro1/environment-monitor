ee5021d04d88eb5a91dbcd63c6327d7a96e66d03
Rafał Białek <bialek.rafal@gmail.com>
2022-01-20 13:44:30 +0100

    Plot for pressure now drawn separately

--- app.py
+++ app.py
@@ -28,11 +28,9 @@ PARAMETER_FORMATS={"temperature":{"format":"0.1f", "suffix":"°C"},
                    "humidity":{"format":"0.0f", "suffix":"%"},
                    "pressure":{"format":"0.0f", "suffix":"hPa"}}
 
-PARAMETER_AXIS_WIDTH={"temperature":0.028,
-                      "humidity":0.028,
-                      "pressure":0.042}
 Y_AXIS_TICKS=5
-PLOT_GRID_COLOR="#333"
+Y_DOMAIN_GAP=0.02
+PLOT_GRID_COLOR="#ccc"
 
 def generate_plot(archive, start, end, parameters):
   print(f"Generating plot from {start} to {end}")
@@ -43,7 +41,7 @@ def generate_plot(archive, start, end, parameters):
   for e in entries:
     print(f"\t{e} from {e.path}")
     e.open()
-    combined_df=combined_df.append(e.dataframe)
+    combined_df=combined_df.append(e.dataframe[start : end])
     e.close()
 
   fig = go.Figure()
@@ -53,19 +51,45 @@ def generate_plot(archive, start, end, parameters):
   # Set general plot layout
   fig.update_layout(
     showlegend=False,
-    margin=dict(t=30, b=0, l=10, r=10), 
     autosize=True,
-    #height=500,
     plot_bgcolor="#fff",
-    dragmode=False,
-    modebar=dict(
-      remove=["pan", "zoom", "zoomin", "zoomout", "autoscale", "resetscale"]
+    hovermode="x"
     )
+
+  # Set X axis layout
+  fig.update_layout(xaxis=dict(
+      title=dict(
+        text="<b>Time</b>",
+        font=dict(
+          size=12
+        )
+      ),
+      side="top",
+      showgrid=True,
+      showspikes=True,
+      spikemode="across",
+      spikethickness=2,
+      gridcolor=PLOT_GRID_COLOR,
+      ticks="outside",
+      tickcolor=PLOT_GRID_COLOR,
+      tickwidth=1,
+      ticklen=10,
+      mirror="ticks",
+      fixedrange=True,
+      rangeslider=dict(
+        visible=True,
+        borderwidth=1,
+      )
+    ),
   )
 
-  y_position=0
+  n_subplots=((len(parameters) + 1) // 2)
+  n_gaps=n_subplots-1
+  subplot_height=(1.0-n_gaps*Y_DOMAIN_GAP)/n_subplots
+
   for i in range(len(parameters)):
     parameter=parameters[i]
+
     # Add trace
     fig.add_trace(go.Scatter(
       x=combined_df.index,
@@ -80,60 +104,54 @@ def generate_plot(archive, start, end, parameters):
     # Y axis calculated properties
     y_axis_min=combined_df[parameter].min()
     y_axis_step=(combined_df[parameter].max()-y_axis_min)/(Y_AXIS_TICKS-1)
+    y_axis_side="left" if i%2==0 else "right"
+    y_axis_overlaying="free" if i%2==0 else f"y{i}" if i!=0 else "y"
+    y_axis_domain_upper=round(1-i//2*(subplot_height+Y_DOMAIN_GAP),3)
+    y_axis_domain_lower=round(y_axis_domain_upper-subplot_height,3)
 
-    # Calculate position of Y axis (right edge)
-    y_position=y_position+PARAMETER_AXIS_WIDTH[parameter]
+    print(f"i:{i} ({PARAMETER_NAMES[parameter]})")
+    print(f"\tmin: {y_axis_min}")
+    print(f"\tstep: {y_axis_step}")
+    print(f"\tside: {y_axis_side}")
+    print(f"\toverlying: {y_axis_overlaying}")
+    print(f"\td_upper: {y_axis_domain_upper}")
+    print(f"\td_lower: {y_axis_domain_lower}")
 
     # Set Y axis layout
     yaxis_layout=dict(
       title=dict(
         text=f"<b>{PARAMETER_NAMES[parameter]}</b>",
-        standoff=0,
         font=dict(
-          size=11
-        )
+          size=12
       ),
-
+        standoff=4
+      ),
       tickfont=dict(
-        size=10
+        size=11
       ),
 
       color=PARAMETER_COLORS[parameter],
+      gridcolor=PLOT_GRID_COLOR,
+      showgrid=True,
+      showspikes=True,
+      spikemode="toaxis",
+      spikethickness=2,
+      fixedrange=True,
 
       tick0=y_axis_min,
       dtick=y_axis_step,
       tickformat=PARAMETER_FORMATS[parameter]["format"],
       ticksuffix=PARAMETER_FORMATS[parameter]["suffix"],
 
-      showgrid=True,
-      gridcolor=PLOT_GRID_COLOR,
-
+      side=y_axis_side,
+      overlaying=y_axis_overlaying,
+      domain=[y_axis_domain_lower, y_axis_domain_upper],
       automargin=True,
-      anchor="free",
-      position=y_position,
-      linewidth=2
+      anchor="x",
     )
-
-    # Set binding of additional Y axes to Y1
-    if i>0:
-      yaxis_layout.update(overlaying="y1")
 
     # Apply layout
     fig.update_layout(**{f"yaxis{i+1}":yaxis_layout})
-
-  # Set X axis layout
-  fig.update_layout(xaxis=dict(
-      title=dict(
-        text="<b>Time</b>",
-        font=dict(
-          size=11
-        )
-      ),
-      domain=[y_position, 1],
-      showgrid=True,
-      gridcolor=PLOT_GRID_COLOR
-    ),
-  )
 
   return fig
 
@@ -141,14 +159,15 @@ app = dash.Dash(__name__)
 app.layout = html.Div(children=[
   dcc.Graph(
     id='graph',
-    responsive=True
+      responsive=True,
+      style={"height":"100%"}
   ),
 
   dcc.Interval(
     id='interval-component',
     interval=20*1000
   )
-])
+], style={"height":"100vh"})
 
 @app.callback(Output('graph', 'figure'),
         Input('interval-component', 'n_intervals'))
