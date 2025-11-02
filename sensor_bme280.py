bd50163adfa5d08b757c1d7f8cc9233a3f442091
Rafał Białek <bialek.rafal@gmail.com>
2025-11-02 13:38:21 +0100

    Add decorations for BME280

--- sensor_bme280.py
+++ sensor_bme280.py
@@ -9,19 +9,74 @@ class Sensor_BME280:
       "name":"Temperature",
       "color":"#EF553B",
       "format":"0.1f",
-      "suffix":"°C"
+      "suffix":"°C",
+      "display":{
+        "custom_chars":[
+          [
+            0b00100,
+            0b01010,
+            0b01010,
+            0b01110,
+            0b11111,
+            0b11111,
+            0b01110,
+            0b00000
+          ],
+          [
+            0b00110,
+            0b01001,
+            0b01001,
+            0b00110,
+            0b00000,
+            0b00000,
+            0b00000,
+            0b00000
+          ]
+        ],
+        "format":"\00{value:4.1f}\01C"
+      }
     },
     "humidity":{
       "name":"Humidity",
       "color":"#636EFA",
       "format":"0.0f",
-      "suffix":"%"
+      "suffix":"%",
+      "display":{
+        "custom_chars":[
+          [
+            0b00100,
+            0b00100,
+            0b01110,
+            0b01110,
+            0b10111,
+            0b11111,
+            0b01110,
+            0b00000
+          ]
+        ],
+        "format":"\00{value:2.0f}%"
+      }
     },
     "pressure":{
       "name":"Pressure",
       "color":"#00CC96",
       "format":"0.0f",
-      "suffix":"hPa"
+      "suffix":"hPa",
+      "display":{
+        "custom_chars":[
+          [
+            0b11010,
+            0b10010,
+            0b11010,
+            0b10010,
+            0b11010,
+            0b10111,
+            0b11010,
+            0b00000
+          ]
+        ],
+        "format":"\00{value:.0f}hPa"
+      }
     },
   }
 
